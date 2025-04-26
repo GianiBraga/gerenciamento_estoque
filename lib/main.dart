de77f1d63f1d6a3e94230024bbfc04a79004c836
Giani Augusto Braga <giani.braga80@gmail.com>
2025-04-26 09:29:05 -0300

    add the supabase config

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import 'package:gerenciamento_estoque/pages/in_out_page.dart';
 import 'package:gerenciamento_estoque/pages/login_page.dart';
 import 'package:gerenciamento_estoque/pages/menu_page.dart';
 import 'package:gerenciamento_estoque/pages/product_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  await Supabase.initialize(
+    url: 'https://wivxkjaospkprnbswoob.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndpdnhramFvc3BrcHJuYnN3b29iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2Njk5NDIsImV4cCI6MjA2MTI0NTk0Mn0.qKhnUuP5l2yLlLVqXSF_pzWwYqDRJSaqNQ3YnD5YiDI',
+  );
   runApp(const MyApp());
 }
 
